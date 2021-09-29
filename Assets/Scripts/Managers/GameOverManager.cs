a09650fd506e7f7c142af5af99fc0e27e825f044
yoesran <19523170naufal@gmail.com>
2021-09-29 20:45:33 +0800

    first

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Text warningText;
+
+    public GameObject screenFader;
+
+    public GameObject warning;
+
+    Animator anim;           
+
+    void Awake()
+    {
+        warning.SetActive(false);
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            
+            screenFader.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+        warning.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public GameOverManager gameOverManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" && !other.isTrigger)
+        {
+            gameOverManager.warning.SetActive(false);
+            float enemyDistance = Vector3.Distance(transform.position, other.transform.position);
+            gameOverManager.ShowWarning(enemyDistance);
+        }
+    }
+}
